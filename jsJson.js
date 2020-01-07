ca8240b2224e1dd5162585648dc11a8edce83b31
RazzSunuwar <razzkoinch5@gmail.com>
2020-01-07 21:27:06 +0545

    Initial Commit

--- jsJson.js
+++ jsJson.js
@@ -37,3 +37,41 @@ send it to a server:*/
 // var myJSON = '{"name": "Linus", "Age": "50", "Address": "Finland"}';
 // var myObj = JSON.parse(myJSON);
 //     document.getElementById("demo").innerHTML = myObj.name;
+
+// Storing Data
+// Storing data in local storage
+
+// Storage data:
+// myObj = {name:"John", age:20, address:"Ktm"};
+// myJSON = JSON.stringify(myObj);
+// localStorage.setItem("testJSON", myJSON);
+
+// // Retriving data:
+// text = localStorage.getItem("testJSON");
+// obj = JSON.parse(text);
+// document.getElementById("demo").innerHTML = obj.name;
+
+// What is JSON?
+// JSON stands for JavaScript Object Notation
+// JSON is a lightweight data-interchange format
+// JSON is "self-describing" and easy to understand
+// JSON is language independent
+
+/*JSON uses JavaScript syntax, but the JSON format is text only. Text can be read and used as a data
+format by any programming language.*/
+
+// The JSON format was originally specified by Douglas Crockford.
+// Note: Don't forget to check the details in the bookmark of google crome browser.
+
+//Why use JSON?
+// Since the JSON format is text only, it can easily be sent to and from a server, and used as a data
+// format by any programming language.
+
+// JavaScript has a built in function to convert a string, written in JSON format, into native JavaScript
+// objects:
+// JSON.parse()
+// So if you receive data from a server, in JSON format, you can use it like any other JavaScript object.
+
+
+
+
