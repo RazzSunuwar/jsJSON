b1b48c3bbe13af5ce7b55e36c4fa4af78f22c843
RazzSunuwar <razzkoinch5@gmail.com>
2020-01-09 21:24:47 +0545

    Documentation

--- jsonJSON.js
+++ jsonJSON.js
@@ -38,7 +38,80 @@
 //     document.getElementById("demo").innerHTML = myObj.name;
 // }  
 
-// Creating a Dynamic JAONP Result
+// Creating a Dynamic Script Tag
+// The example above will execute the "myFunc" function when the page is loading, based on where you put the script tag, which is not very satisfying.
+// The script tag should only be created when needed:
+// Example
+// Create and insert the <script> tag when a button is clicked:
+
+// function clickButton() {
+//     var s = document.createElement("script");
+//     s.src = "demo_jsonp.php";
+//     document.body.appendChild(s);
+// }
+  
+// function myFunc(myObj) {
+//     document.getElementById("demo").innerHTML = myObj.name;
+// }
+
+// Creating a Dynamic JSONP Result
 // The examples above are still very static.
 // Making the example dynamic by sending JSON to the php file, and let the php file, and
 // let the php file return a JSON object based on the informations it gets.
+
+// PHP code
+// <?php
+// header("Content-Type: application/json; charset=UTF-8");
+// $obj = json_decode($_GET["x"], false);
+
+// $conn = new mysqli("myServer", "myUser", "myPassword", "Northwind");
+// $result = $conn->query("SELECT name FROM ".$obj->$table." LIMIT ".$obj->$limit);
+// $outp = array();
+// $outp = $result->fetch_all(MYSQLI_ASSOC);
+
+// echo "myFunc(".json_encode($outp).")";
+// ?>
+
+// PHP File explained:
+// Convert the request into an object, using the PHP function json_decode().
+// Access the database, and fill an array with the requested data.
+// Add the array to an object.
+// Convert the array into JSON using the json_encode() function.
+// Wrap "myFunc()" around the return object.
+
+// JavaScript Example
+// The "myFun" function will be called from the php file:
+// function clickButton() {
+//     var obj, s
+//     obj = { table: "customers", limit: 10 };
+//     s = document.createElement("script");
+//     s.src = "jsonp_demo_db.php?x=" + JSON.stringify(obj);
+//     document.body.appendChild(s);
+//   }
+  
+//   function myFunc(myObj) {
+//     var x, txt = "";
+//     for (x in myObj) {
+//       txt += myObj[x].name + "<br>";
+//     }
+//     document.getElementById("demo").innerHTML = txt;
+// }
+
+// Callback Function
+// When you have no control over the server file, how do you get the server file to call
+// the correct function?
+// Sometimes the server file offers a callback function as a parameter:
+
+// Example
+// The php file will call the function you pass as a callback parameter:
+// function clickButton() {
+//     var s = document.createElement("script");
+//     s.src = "demo_jsonp2.php?callback=myDisplayFunction";
+//     document.body.appendChild(s);
+// }
+  
+// function myDisplayFunction(myObj) {
+//     document.getElementById("demo").innerHTML = myObj.name;
+// }
+
+
